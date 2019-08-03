eb00a2ce5b7a88d36b22ce07b34c6f6c66b8c6df
Sean McPherson <seanmcp25@gmail.com>
2019-08-03 13:19:07 -0400

    Remove key validation

--- src/validate.js
+++ src/validate.js
@@ -1,38 +1,21 @@
-import KEY from 'all-keyboardevent-keys'
-
-export function isObject(input) {
-  return typeof input === 'object' && input !== null
-}
-
-export function areValidKeys(keyActionMap, options) {
-  if (options && options.skipKeyValidation) {
-    return
-  }
-  for (const key in keyActionMap) {
-    if (!KEY.hasOwnProperty(key)) {
-      throw Error(
-        `'${key}' is not a valid KeyboardEvent key (To override this check, pass the 'skipKeyValidation' option: https://www.npmjs.com/package/onkey-event-manager#options)`
-      )
-    }
-  }
-}
+import isObject from 'isobject'
 
 export function areValidFunctions(keyActionMap) {
   for (const key in keyActionMap) {
     if (typeof keyActionMap[key] !== 'function') {
-      throw Error(`The value for '${key}' must be a function`)
+      throw TypeError(`The value for '${key}' must be a function`)
     }
   }
 }
 
 export function areValidParams(keyActionMap, options) {
   if (!isObject(keyActionMap)) {
-    throw Error(
+    throw TypeError(
       `The first argument passed to 'onKey' must be an object of valid KeyboardEvent keys and callback functions`
     )
   }
   if (typeof options !== 'undefined' && !isObject(options)) {
-    throw Error(
+    throw TypeError(
       `The second argument passed to 'onKey' must be an object of valid options`
     )
   }
@@ -40,6 +23,5 @@ export function areValidParams(keyActionMap, options) {
 
 export default function() {
   areValidParams(...arguments)
-  areValidKeys(...arguments)
   areValidFunctions(...arguments)
 }
--- src/validate.test.js
+++ src/validate.test.js
@@ -1,30 +1,12 @@
 import validate from './validate'
 
-const params = {
-  valid: {
-    key: 'ArrowDown',
-    function: () => null,
-    options: { skipKeyValidation: true }
-  },
-  invalid: {
-    key: 'Bananas',
-    function: ''
-  }
-}
+const KEY = 'ArrowDown',
+  VALID_FUNCTION = () => null,
+  INVALID_FUNCTION = ''
 
 test('validate succeeds with valid key map', () => {
   function wrapper() {
-    validate({ [params.valid.key]: params.valid.function })
-  }
-  expect(wrapper).not.toThrowError()
-})
-
-test('validate succeeds with invalid key AND skipKeyValidation', () => {
-  function wrapper() {
-    validate(
-      { [params.invalid.key]: params.valid.function },
-      params.valid.options
-    )
+    validate({ [KEY]: VALID_FUNCTION })
   }
   expect(wrapper).not.toThrowError()
 })
@@ -43,16 +25,9 @@ test('validate errors with invalid options', () => {
   expect(wrapper).toThrowError()
 })
 
-test('validate errors with invalid key', () => {
-  function wrapper() {
-    validate({ [params.invalid.key]: params.valid.function })
-  }
-  expect(wrapper).toThrowError()
-})
-
 test('validate errors with invalid function', () => {
   function wrapper() {
-    validate({ [params.valid.key]: params.invalid.function })
+    validate({ [KEY]: INVALID_FUNCTION })
   }
   expect(wrapper).toThrowError()
 })
